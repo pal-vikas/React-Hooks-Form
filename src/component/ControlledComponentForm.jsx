1bd098b3d5a53b909e4c2351ca57d547f442c72d
Vikas Pal <vikaspal@Vikass-MacBook-Air.local>
2025-07-08 23:00:39 +0530

    React Hook Form

--- src/component/ControlledComponentForm.jsx
+++ src/component/ControlledComponentForm.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form"
+
 
 function ControlledComponentForm() {
 
-    let [value, setValue] = useState({ name: "", email: "" })
+   const {register, handleSubmit, reset} = useForm();
 
-    function submitHandler(event) {
-        event.preventDefault();
-        console.log(value);
-        setValue({ name: "", email: "" });
-
-    }
+   function submitHandler(data)
+   {
+        console.log(data);
+        reset();
+   }
 
     return (
         <div>
             <div className="mx-auto flex h-screen w-full items-center justify-center">
-                <form action="" className="w-[600px] border border-neutral-400 p-10 rounded-lg" onSubmit={submitHandler}>
+                <form action="" onSubmit={handleSubmit(submitHandler)} className="w-[600px] border border-neutral-400 p-10 rounded-lg" >
 
                     <div className="text-center mb-2">
-                        <h1 className="text-2xl font-bold">Controlled Components forms</h1>
+                        <h1 className="text-2xl font-bold">React Hooks forms</h1>
                         <p className="text-sm text-neutral-500">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aperiam soluta, ipsa quam commodi dicta.
                         </p>
@@ -25,8 +26,8 @@ function ControlledComponentForm() {
 
 
                     <div className="flex justify-center gap-5 mt-">
-                        <input type="text" className="border rounded-lg p-2 outline-violet-200 " onChange={(event) => setValue({ ...value, name: event.target.value })} placeholder='Enter the Name' />
-                        <input type="email" className="border rounded-lg p-2 outline-violet-200 " onChange={(event) => setValue({ ...value, email: event.target.value })} placeholder='Enter the Email' />
+                        <input type="text" {...register ('name')} className="border rounded-lg p-2 outline-violet-200 " placeholder='Enter the Name' />
+                        <input type="email" {...register ('email')} className="border rounded-lg p-2 outline-violet-200 "  placeholder='Enter the Email' />
                     </div>
                     <div className="mx-auto flex items-center justify-center">
                         <input className="my-5 rounded-lg bg-black text-white py-2 px-4" type="submit" />
