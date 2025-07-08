90d04ac6545cf91e600a2a8682263e0595396221
Vikas Pal <vikaspal@Vikass-MacBook-Air.local>
2025-07-08 22:46:33 +0530

    Controlled Components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ControlledComponentForm from "./component/ControlledComponentForm"
+import Vik from "./component/Vik"
+
+function App() {
+
+  return (
+    <>
+        <ControlledComponentForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Vik.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Vik() {
+  return (
+    <div className='text-2xl'>
+            <h1>Vikas PAL</h1>
+    </div>
+  )
+}
+
+export default Vik
